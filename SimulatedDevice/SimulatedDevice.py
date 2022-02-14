8b3eaae6e5124ebef0e542d74f263e094eb020e0
sobyandrew <sobyandrew@gmail.com>
2022-02-14 14:52:28 -0800

    simulated device

--- SimulatedDevice/SimulatedDevice.py
+++ SimulatedDevice/SimulatedDevice.py
@@ -1,20 +1,34 @@
 from uuid import UUID, uuid4
 import paho.mqtt.client as mqtt
 import threading
+import csv
+import datetime
+import time
 from time import sleep
+from random import randrange
 
-def getMessage():
-    return "{ \"uuid\": \"" + str(uuid4()) + "\"}"
-# This is the Publisher
-def send(client, topic):
-    print("in send")
-    # client = mqtt.Client(str(uuid4()), True)
-    # print("in send2")
-    # client.connect("localhost",443,60)
-    # print("in send3")
-    client.publish(topic, getMessage(), qos=0);
+numSimulatedDevices = 100
+numMessagesPerDevice = 100
+file = open('log_temp.csv')
+
+csvreader = csv.reader(file)
+
+header = next(csvreader)
+rows = []
+for row in csvreader:
+    rows.append(row)
+file.close()
+
+def getMessage(clientNum):
+    randRow = randrange(len(rows))
+    dataString = "{\"temperature\":" + "\"" + rows[randRow][0]+ "\""+ ", \"humidity\":" + "\"" + rows[randRow][1]+ "\"" + "}"
+    retStr = "{\"deviceId\":\""  + clientNum + "\", \"time\":\"" + str(datetime.datetime.now()) +"\", \"data\":" + dataString+ "}"
+    return retStr
+
+def send(client, topic, clientNum):
+    client.publish(topic, getMessage(clientNum), qos=0)
     
-def createClientAndCallSend():
+def createClientAndCallSend(clientNum):
     clientUUID = "client" + str(uuid4())
     cli =  mqtt.Client(clientUUID, True)
     cli.connect("localhost", 443, 60)
@@ -22,34 +36,24 @@ def createClientAndCallSend():
     count = 0
     while x:
         count = count + 1
-        send(cli, clientUUID)
-        sleep(0.0001)
-        if count >= 1:
+        send(cli, clientUUID, clientNum)
+        sleep(0.001)
+        if count >= numMessagesPerDevice:
             return 
-    
-threads = []
 
-for n in range(1):
-    t = threading.Thread(target=createClientAndCallSend, args=())
+threads = []
+start_time = datetime.datetime.now()
+
+for n in range(numSimulatedDevices):
+    t = threading.Thread(target=createClientAndCallSend, args=(str(n),))
     t.start()
     threads.append(t)
 
 for t in threads:
     t.join()
-# print("before send")
+end_time = datetime.datetime.now()
 
-# print("after send")
-# client2 = mqtt.Client(str(uuid4()), True)
-# client2.connect("localhost",443,60)
-# send(client2)
-# count = 0
-# x = True
-# while x:
-#     # client = mqtt.Client(str(uuid4()), True)
-#     # client.connect("localhost",443,60)
-#     # client.publish("test", "test world 234234 234234234 234 234 23 4334", qos=0);
-#     print("creating client")
-#     createClientAndCallSend()
-#     count = count + 1
-#     if count > 5:
-#         x = False
+time_diff = (end_time - start_time)
+execution_time = time_diff.total_seconds() * 1000
+print(execution_time)
+print("total")
