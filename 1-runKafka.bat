93ba7278c8d3b130c9610253672a8186e44e5f57
sobyandrew <sobyandrew@gmail.com>
2022-05-05 15:36:15 -0700

    Update 1-runKafka.bat

--- 1-runKafka.bat
+++ 1-runKafka.bat
@@ -1,5 +1,5 @@
 @echo off
 cd C:\kafka
 start "zookeeperInstance" .\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
-timeout /t 10 /nobreak > NUL
+timeout /t 15 /nobreak > NUL
 start "kafkaInstance" .\bin\windows\kafka-server-start.bat .\config\server.properties
